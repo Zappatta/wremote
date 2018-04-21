038fcc252520bf8a33c2b06b6979993364078b2b
yoskesh <yoske@zencity.io>
2018-04-21 11:19:22 +0300

    short doc change

--- server/src/config.sample.js
+++ server/src/config.sample.js
@@ -1,5 +1,5 @@
 export const PORT = 8080;
 export const AUTHORIZED_GOOGLE_EMAILS = ['You@example.com'];
 export const GOOGLE_CLIENT_ID  = "YOUR_GOOGLE_CLIENT_TOKEN";
-export const G_ID_TOKEN_COOKIE_NAME = 'g_id_token';
+export const G_ID_TOKEN_COOKIE_NAME = 'g_id_token'; //probably no need to change this one unless google changes their cookie name
 export const AUTHORIZED_ARDIUNO_DNS_NAME = 'your.dynamic.dns';//see ArduinoAuth.js
