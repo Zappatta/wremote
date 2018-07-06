4a0c6002ab95eeff4f3257fdc1fb3f6760254259
yoskesh <yoske@zencity.io>
2018-07-06 16:09:41 +0300

    Use MQTT to talk to server

--- arduino/config.sample.h
+++ arduino/config.sample.h
@@ -1,6 +1,9 @@
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASS";
-const String host = "your.server.com";
 const uint8_t TEMP_SENSOR_PIN = A0;//change according to your arduino board and how you built it
 const uint8_t IR_LED_PIN = D6; 
 const uint8_t STATUS_LED = 14;
+#define MQTT_SERVER    	"your.mqtt.server"
+#define MQTT_USERNAME  	"YOUR_MQTT_USER"
+#define MQTT_PASS       "YOUR_MQTT_PASS"
+#define MQTT_SUBJECT 	"/subjectsample/"
