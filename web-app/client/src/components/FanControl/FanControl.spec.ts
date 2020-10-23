7a4e9d281ade0dd53541a1820438ef1f459ed737
Yoske <zappatta@gmail.com>
2020-10-23 22:03:34 +0300

    FanControl tests done

--- web-app/client/src/components/FanControl/FanControl.spec.ts
+++ web-app/client/src/components/FanControl/FanControl.spec.ts
@@ -5,7 +5,7 @@ import Vuetify from 'vuetify'
 
 Vue.use(Vuetify)
 
-describe('FanControl: Buttons emit changeevent', () => {
+describe('FanControl: Buttons emit change event', () => {
   const wrapper = mount(FanControl);
 
   test('button 1', async ()=> {
@@ -31,5 +31,57 @@ describe('FanControl: Buttons emit changeevent', () => {
     await button4.trigger('click');
     expect(wrapper.emitted().change).toContainEqual([3]);
   });
-
 })
+
+describe('FanControl: State changes css', () => {
+  const wrapper = mount(FanControl);
+  const button1 = wrapper.find('.button-1');
+  const button2 = wrapper.find('.button-2');
+  const button3 = wrapper.find('.button-3');
+  const button4 = wrapper.find('.button-4');
+
+  test('button 1', async ()=> {
+    await wrapper.setProps({
+      value: 0
+    })
+
+    expect(button1.classes('v-btn--active')).toBe(true);
+    expect(button2.classes('v-btn--active')).toBe(false);
+    expect(button3.classes('v-btn--active')).toBe(false);
+    expect(button4.classes('v-btn--active')).toBe(false);
+  });
+
+  test('button 2', async ()=> {
+    await wrapper.setProps({
+      value: 1
+    })
+
+    expect(button1.classes('v-btn--active')).toBe(false);
+    expect(button2.classes('v-btn--active')).toBe(true);
+    expect(button3.classes('v-btn--active')).toBe(false);
+    expect(button4.classes('v-btn--active')).toBe(false);
+  });
+
+  test('button 3', async ()=> {
+    await wrapper.setProps({
+      value: 2
+    })
+
+    expect(button1.classes('v-btn--active')).toBe(false);
+    expect(button2.classes('v-btn--active')).toBe(false);
+    expect(button3.classes('v-btn--active')).toBe(true);
+    expect(button4.classes('v-btn--active')).toBe(false);
+  });
+
+  test('button 4', async ()=> {
+    await wrapper.setProps({
+      value: 3
+    })
+
+    expect(button1.classes('v-btn--active')).toBe(false);
+    expect(button2.classes('v-btn--active')).toBe(false);
+    expect(button3.classes('v-btn--active')).toBe(false);
+    expect(button4.classes('v-btn--active')).toBe(true);
+  });
+
+});
